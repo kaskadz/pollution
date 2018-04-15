e7303de96069fb5fae8a3153055ecb1cc7085b64
Kasper Kądzielawa <kasper.kadzielawa@gmail.com>
2018-04-15 21:37:39 +0200

    Add add type and record definitions.

---
+++ include/pollution.hrl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @author Kasper
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15. Apr 2018 19:46
+%%%-------------------------------------------------------------------
+-author("Kasper").
+
+-ifndef(POLLUTION_HRL).
+-define(POLLUTION_HRL, 1).
+
+
+-type station_name() :: string().
+-type coords() :: {float(), float()}.
+-type timestamp() :: calendar:datetime().
+-type m_type() :: string().
+-type measurement() :: {m_type(), timestamp(), float()}.
+
+
+-record(station, {
+  name :: station_name(),
+  coordinates :: coords(),
+  measurements = [] :: [measurement()]
+}).
+-type station() :: #station{}.
+
+-record(monitor, {
+  stations = #{} :: #{station_name() => #station{}},
+  coords_to_station_name = #{} :: #{coords() => station_name()}
+}).
+-type monitor() :: #monitor{}.
+
+
+-endif.
